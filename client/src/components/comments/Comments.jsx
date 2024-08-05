0755eb47576d14658d7647f3cc64a9ae99443c5c
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-05 23:18:32 +0300

    Adding Comments component

---
+++ client/src/components/comments/Comments.jsx
@@ -0,0 +1,26 @@
+function Comments() {
+    return (
+        <>
+            <div className='com-container'>
+                <span>Username: </span>
+                <p>I think it's beautiful !</p>
+                <br />
+            </div>
+            <div className='com-container'>
+                <span>Username: </span>
+                <p>That is some solid gameeee! I am waiting for patch 3.0 to come out!!</p>
+            </div>
+            <div className='com-container'>
+                <span>Username: </span>
+                <p>That is some solid gameeee! I am waiting for patch 3.0 to come out!! Blaldlwdl wpdla d  w d</p>
+            </div>
+            <div className='com-container'>
+                <span>Username: </span>
+                <p>I think it's beautiful !</p>
+            </div>
+
+        </>
+    );
+}
+
+export default Comments;
