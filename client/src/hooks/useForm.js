0a1649ddda2b048cfdc551bbafbbc13df3dfbf33
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-09 02:24:58 +0300

    Add some notes & comments to code

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -10,17 +10,19 @@ export function useForm(initialValues, submitCallback) {
     const [values, setValues] = useState(initialValues);
 
     // TODO: Add support for checkbox
-    const changeHandler = (e) => {       
+    const changeHandler = (e) => {       // saves EVERY SINGLE INPUT from from fields in a state
         setValues(prevState => ({
             ...prevState,
             [e.target.name]: e.target.value,
         }));
     }
 
+    // Then, the data collected from 'changeHadler' must be 'submitted' (send to server)
+    // we create this func ONLY to prevent page refresh. Otherwise it does exactly the same as 'submitCallBack' (SUBMITs the form)
     const submitHandler = (e) => {
         e.preventDefault();
         submitCallback(values);
-        // console.log(values);
+        console.log(values);
         
     }
     return {
