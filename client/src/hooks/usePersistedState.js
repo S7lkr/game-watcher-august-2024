0b2001efd3542d33084b606cff025cb187dd76f8
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-11 11:58:20 +0300

    Edit usePersistedState -> shorter code

--- client/src/hooks/usePersistedState.js
+++ client/src/hooks/usePersistedState.js
@@ -4,20 +4,21 @@ export default function usePersistedState(key, initialState) {      // prevents 
     const [state, setState] = useState(() => {
         const persistedAuth = localStorage.getItem(key);
         if (!persistedAuth) {
-            return typeof(initialState) === 'function'  // 'state' wil be either this
-                ? initialState()
-                : initialState
+            return typeof(initialState) === 'function' ? initialState() : initialState;     // 'state' wil be either initial
         };
         const authData = JSON.parse(persistedAuth);
-        return authData;    // or this
+        return authData;                                                                    // or this
     });
 
     const updateState = (value) => {
-        const newState = typeof(value) === 'function'
-            ? value(state)
-            : value;
-        localStorage.setItem(key, JSON.stringify(newState));   // update localStorage
-        setState(newState);                                    // and update state
+        const newState = typeof(value) === 'function' ? value(state) : value;
+        
+        if (newState === null || newState === undefined) {
+            localStorage.removeItem(key);
+        } else {
+            localStorage.setItem(key, JSON.stringify(newState));    // update localStorage
+        }
+        setState(newState);                                         // and update state
     }
     return [state, updateState];
 }
