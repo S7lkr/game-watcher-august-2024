a2aa617dc761a04cff8f8d6d385166d7c836a593
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-11 11:57:30 +0300

    Fix logout comp -> avoid mount trouble

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,9 +1,12 @@
 import { Navigate } from "react-router";
 import { useLogout } from "../../hooks/useAuth";
+import { useEffect } from "react";
 
 export default function Logout() {
     const logout = useLogout();
-    logout();
+    useEffect(() => {           // logout hook will be invoked ONLY on component mount!
+        logout();
+    }, []);
     
     return (
         <Navigate to={'/'} />
