d2a3de9470618164c136d4e93aa45c43e7fa31b1
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-14 18:33:57 +0300

    Create Devs sub-component for OurTeam

---
+++ client/src/components/about-our-team/about-our-team-devs/OurTeamDevs.jsx
@@ -0,0 +1,97 @@
+export default function OurTeamDevs() {
+    return (
+        <div className="row">
+            <div className="col-lg-3 mb-4">
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                    </div>
+                    <div className="col-md-12 text-center">
+                        <div className="pt-2">
+                            <h5 className="mt-4 font-weight-medium mb-0">Eminem</h5>
+                            <h6 className="subtitle mb-3">Lead Programmer</h6>
+                            <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-behance"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="col-lg-3 mb-4">
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t2.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                    </div>
+                    <div className="col-md-12 text-center">
+                        <div className="pt-2">
+                            <h5 className="mt-4 font-weight-medium mb-0">Britney Spears</h5>
+                            <h6 className="subtitle mb-3">Graphics Designer</h6>
+                            <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-behance"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="col-lg-3 mb-4">
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t3.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                    </div>
+                    <div className="col-md-12 text-center">
+                        <div className="pt-2">
+                            <h5 className="mt-4 font-weight-medium mb-0">Frank Sinatra</h5>
+                            <h6 className="subtitle mb-3">3D Models Specialist</h6>
+                            <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-behance"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="col-lg-3 mb-4">
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t4.jpg" alt="wrapkit" className="img-fluid rounded-circle" />
+                    </div>
+                    <div className="col-md-12 text-center">
+                        <div className="pt-2">
+                            <h5 className="mt-4 font-weight-medium mb-0">Shakira</h5>
+                            <h6 className="subtitle mb-3">Textures & UI Specialist</h6>
+                            <p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="#" className="text-decoration-none d-block px-1"><i className="icon-social-behance"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
