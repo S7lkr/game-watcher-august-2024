8d1e3700b298f10ef90ecf0a0afc56b28dc33625
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-14 18:32:56 +0300

    Create ContactUs component & route

---
+++ client/src/components/about-contact-us/ContactUs.jsx
@@ -0,0 +1,26 @@
+export default function ContactUs() {
+    return (
+        <div className="contact-page section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 align-self-center">
+                        <div className="left-text">
+                            <div className="section-heading">
+                                <h6>Contact Us</h6>
+                                <h2>GameWatcher - Watch for every game!</h2>
+                            </div>
+                            <p>Driven by passion, we are a global leader in digital interactive entertainment.</p>
+                            <p>Headquartered in Sunny Isles Beach, Miami Beach, Florida, we develop and deliver games, content, and online services for Internet-connected consoles, mobile devices, and personal computers. High-quality brands such as EA SPORTS FC™, Battlefield™, Apex Legends™, The Sims™, Madden NFL, Need for Speed™, Titanfall® and F1®.</p>
+                            <ul>
+                                <li><img src="https://www.livebeaches.com/wp-content/uploads/2024/02/florida-miami-beach-map-beaches-900x715-1.png" alt="" /></li>
+                                <li><span>Address</span> Sunny Isles Beach, FL 33160, United States</li>
+                                <li><span>Phone</span> +123 456 7890</li>
+                                <li><span>Email</span> game_watcher@contact.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
