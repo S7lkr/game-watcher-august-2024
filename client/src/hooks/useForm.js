29b8aa88f2273364cb3d15e472afec49c1094874
S7lkr <s.7.a.l.k.3.r.8.9@gmail.com>
2024-08-15 23:52:07 +0300

    Add some comments in useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
  * @param {function} submitCallback 
  * @returns 
  */
+// 'submitCallBack' performs SOMETHING on submit. It is pre-defined in the comp using useForm() hook
+//                                          v
 export function useForm(initialValues, submitCallback) {
     const [values, setValues] = useState(initialValues);
 
@@ -14,11 +16,11 @@ export function useForm(initialValues, submitCallback) {
     }, [initialValues]);        // will re-render on 'initialValues' change
     
     // TODO: Add support for checkbox
-    const changeHandler = (e) => {       // saves EVERY SINGLE INPUT from from fields in a state
+    const changeHandler = (e) => {       // saves EVERY SINGLE INPUT from fields into a state (values)
         setValues(prevState => ({
             ...prevState,
             [e.target.name]: e.target.value,
-        }));
+        }));        
     }
 
     // Then, the data collected from 'changeHadler' must be 'submitted' (send to server)
@@ -26,7 +28,7 @@ export function useForm(initialValues, submitCallback) {
     const submitHandler = async (e) => {
         e.preventDefault();
         await submitCallback(values);     // save values in state
-        setValues(initialValues);   // clear fields
+        setValues(initialValues);         // clear fields
     }
     
     return {
